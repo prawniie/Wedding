fa1c7b5193ac8caba0d2a9209496220d9220f2db
rebeckalscarlsson <rebecka.ls.carlsson@gmail.com>
2019-01-21 11:04:12 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+window.alert('Välkommen till vår bröllopssida!');
+
+function changeHeaderText() {
+    alert('hmm')
+    document.getElementById("minRubrik").innerHTML = "Mindre än fyra månader kvar!";
+}
