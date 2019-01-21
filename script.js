8b9af64e9f9bd60706132a19b5b8ed24098abe41
rebeckalscarlsson <rebecka.ls.carlsson@gmail.com>
2019-01-21 12:33:45 +0100

    no message

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-window.alert('Välkommen till vår bröllopssida!');
+//window.alert('Välkommen till vår bröllopssida!');
 
 function changeHeaderText() {
-    alert('hmm')
-    document.getElementById("minRubrik").innerHTML = "Mindre än fyra månader kvar!";
+    document.getElementById("tidKvar").innerHTML = "Mindre än fyra månader kvar!";
 }
+
+
+//console.log för att skriva ut saker 
