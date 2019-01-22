ad8899ffd745c96b2c66cf835a62e59c05a64c15
rebeckalscarlsson <rebecka.ls.carlsson@gmail.com>
2019-01-22 09:50:45 +0100

    Lagt till knappar på olika sätt som skriver ut lite grejer

--- script.js
+++ script.js
@@ -4,5 +4,44 @@ function changeHeaderText() {
     document.getElementById("tidKvar").innerHTML = "Mindre än fyra månader kvar!";
 }
 
+//annat sätt:
+let tidKvar = document.getElementById("tidKvar");
+tidKvar.innerText = "Ännu mindre än fyra månader kvar!";
+
+//innerText tolkar bokstavligt till skillnad från innerHTML
+
+//changeHeaderText(); kan också kalla på en funktion bara
 
 //console.log för att skriva ut saker 
+
+function printSomething() {
+    alert('Hej');
+}
+
+function printOutInConsole() {
+    console.log('Hej i consolen');
+}
+
+let knapp2 = document.getElementById("knapp02");
+knapp2.addEventListener("click", printSomething);
+
+let knapp4 = document.getElementById("knapp04");
+knapp4.addEventListener("click", printOutInConsole);
+
+let knapp5 = document.getElementById("knapp05");
+knapp5.addEventListener("click", () => {
+    console.log('Femte knappen nu..');
+});
+
+//istället för click kan man köra andra kommandon typ tex mouseover, mouseleave
+
+//Olika sätt att göra funktioner:
+function testy() {
+    alert('xx')
+}
+testy();
+
+let testy2 = () => alert('xxxxx');
+testy2(); //Går att testa i browsern också
+
+
